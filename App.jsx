26dd7a1ba9744faf7673df785fac77f65d017d2d
Rahul Kumar <97456833+Gitadmasai@users.noreply.github.com>
2022-06-01 12:55:01 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,11 @@
+import "./module.css";
+import { ContactList } from "./components/ContactList";
+function App() {
+  return (
+    <div className="App">
+      <ContactList />
+    </div>
+  );
+}
+
+export default App;
